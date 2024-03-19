6562ff1abf801c642316f7017f7d17c95bb0ef11
Artemiy <alistan123@bk.ru>
2024-03-19 20:08:05 +0400

    Добавьте файлы проекта.

---
+++ SquareTestLib.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SquareTestLib.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
